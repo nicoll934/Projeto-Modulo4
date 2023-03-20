b4818b66532b6d2370bfc81dc0f5e180f0686bc0
nicolly ketlyn <nicollyketlyn97@gmail.com>
2023-03-20 15:59:07 -0300

    feat banco de dados pronto

---
+++ soscamp/src/main/java/com/bjlngroup/soscamp/models/PagamentoRequest.java
@@ -0,0 +1,22 @@
+package com.bjlngroup.soscamp.models;
+
+public class PagamentoRequest {
+    private long cliente_id;
+    private long plano_id;
+
+    public long getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(long cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public long getPlano_id() {
+        return plano_id;
+    }
+
+    public void setPlano_id(long plano_id) {
+        this.plano_id = plano_id;
+    }
+}
